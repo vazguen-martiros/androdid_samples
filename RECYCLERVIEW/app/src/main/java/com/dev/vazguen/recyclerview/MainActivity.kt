ebc0c7147292d7e49e45d51ddcb90955f3cf1c7d
Vazgen <marvazgen@gmail.com>
2022-12-22 23:05:24 +0100

    update recylerview project

--- RECYCLERVIEW/app/src/main/java/com/dev/vazguen/recyclerview/MainActivity.kt
+++ RECYCLERVIEW/app/src/main/java/com/dev/vazguen/recyclerview/MainActivity.kt
@@ -2,10 +2,20 @@ package com.dev.vazguen.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.dev.vazguen.recyclerview.adapter.SuperHeroAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initRecylerView()
     }
+     fun initRecylerView(){
+         val recyclerView = findViewById<RecyclerView>(R.id.recylerSuperHero)
+         recyclerView.layoutManager = LinearLayoutManager(this)
+         recyclerView.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList)
+     }
 }
